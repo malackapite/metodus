eeb6a66425d64670ebd145c6bf0cb127b280bbff
malackapite <veress.marcell@protonmail.com>
2023-02-22 15:09:58 +0100

    metodus gyakorlas

--- src/gyakorlas/Metodusgyak.java
+++ src/gyakorlas/Metodusgyak.java
@@ -6,10 +6,17 @@ public class Metodusgyak {
         String szoveg="Az első tíz szám összege: " + elso10SzamOssege();
         kiIr(szoveg);
         
-        int a=3, b=5;
+        int a=3, b=5, c, d;
         szoveg=String.format("%d + %d = ", a, b) + osszead(a, b);
         kiIr(szoveg);
         
+        a=2;b=3;c=3;d=5;
+        szoveg="4 szám összege: " + osszead(a, osszead(b, osszead(c, d)));
+        kiIr(szoveg);
+        
+        a=3;b=4;c=2;
+        szoveg="%d+%d+%d gyöke: %.0f".formatted(a,b,c, Math.sqrt(osszead(a, osszead(b, c))));
+        kiIr(szoveg);
     }
     
     private static int elso10SzamOssege(){
